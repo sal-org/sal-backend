6d1bbc4b2087ef6d09c39618b47858013896341b
offlineforce <preeti.dave@salesforce.com>
2020-11-18 12:34:26 -0800

    aa

--- aws/handlers/appoinments_api_req_handler.go
+++ aws/handlers/appoinments_api_req_handler.go
@@ -3,8 +3,6 @@ package handlers
 import (
 	"net/http"
 
-	"github.com/sal-org/sal-backend/counselor"
-
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/sal-org/sal-backend/aws/database"
@@ -15,23 +13,33 @@ import (
 
 // HandleAppointmentsRequest will handle all appointment gate api requests
 func HandleAppointmentsRequest(req events.APIGatewayProxyRequest) (*events.APIGatewayProxyResponse, error) {
-	db, err := database.CreateDB()
 
-	if err != nil {
-		return utils.GatewayResponse(http.StatusBadRequest, models.ErrorBody{ErrorMsg: aws.String(err.Error())})
+	switch req.HTTPMethod {
+	case "GET":
+		db, err := database.CreateDB()
+
+		if err != nil {
+			return utils.GatewayResponse(http.StatusBadRequest, models.ErrorBody{ErrorMsg: aws.String(err.Error())})
+		}
+
+		repository := repositories.AppointmentsRepository{DB: db}
+		counselors, err := repository.FetchAll(nil)
+		return utils.GatewayResponse(http.StatusOK, counselors)
+	case "POST":
+		db, err := database.CreateDB()
+
+		if err != nil {
+			return utils.GatewayResponse(http.StatusBadRequest, models.ErrorBody{ErrorMsg: aws.String(err.Error())})
+		}
+
+		repository := repositories.AppointmentsRepository{DB: db}
+		counselor, err := repository.CreateAppointment(req)
+		if err != nil {
+			return utils.GatewayResponse(http.StatusBadRequest, models.ErrorBody{ErrorMsg: aws.String(err.Error())})
+		}
+
+		return utils.GatewayResponse(http.StatusOK, counselor)
+	default:
+		return utils.GatewayResponse(http.StatusBadRequest, models.ErrorBody{ErrorMsg: aws.String("Method not allowed")})
 	}
-
-	repository := repositories.AppointmentsRepository{DB: db}
-
-	//TODO
-	counselor := counselor.Counselor{
-		Identifier: "random",
-	}
-
-	appointments, err := repository.FetchAll(&counselor)
-	if err != nil {
-		return utils.GatewayResponse(http.StatusBadRequest, models.ErrorBody{ErrorMsg: aws.String(err.Error())})
-	}
-
-	return utils.GatewayResponse(http.StatusOK, appointments)
 }
