2ef8a953ff1d450266210cea46e5a8f0392c78e4
offlineforce <preeti.dave@salesforce.com>
2020-11-27 18:54:42 -0800

    fix appointments handler

--- aws/handlers/appointments_api_req_handler.go
+++ aws/handlers/appointments_api_req_handler.go
@@ -51,21 +51,6 @@ func HandleAppointments1Request(req events.APIGatewayProxyRequest) (*events.APIG
 		return utils.GatewayResponse(http.StatusOK, appointment)
 	
 	default:
-		db, err := database.CreateDB()
-		log.Printf("GET: %s", req.HTTPMethod)
-		if err != nil {
-			return utils.GatewayResponse(http.StatusBadRequest, models.ErrorBody{ErrorMsg: aws.String(err.Error())})
-		}
-
-		repository := repositories.AppointmentsRepository{DB: db}
-		//TO-DO this is work in progress need to add api to get appointments per user/counselor/timeslot
-		counselorId  := req.QueryStringParameters["counselor_id"]
-		appointments, err := repository.FetchAll("1")
-		if err != nil {
-				return utils.GatewayResponse(http.StatusBadRequest, models.ErrorBody{ErrorMsg: aws.String(err.Error())})
-		}
-		log.Printf("Fetchall: %s", counselorId)
-		
-		return utils.GatewayResponse(http.StatusOK, appointments)	
+		return utils.GatewayResponse(http.StatusBadRequest, models.ErrorBody{ErrorMsg: aws.String("Method not allowed")})	
 	}
 }
